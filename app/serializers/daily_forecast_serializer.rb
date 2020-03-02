3fcde0e335f25830f11cbeebd976a63a77c747e9
Madelyn Romero <m.romero013@gmail.com>
2020-03-02 07:17:17 -0700

    Add eight hour temperatures

--- app/serializers/daily_forecast_serializer.rb
+++ app/serializers/daily_forecast_serializer.rb
@@ -1,7 +1,7 @@
 class DailyForecastSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :id, :weather_icon, :current_summary, :current_temperature, :city_state, :country, :time, :today_forecast, :tonight_forecast, :feels_like, :humidity, :visibility, :uv_index
+  attributes :id, :weather_icon, :current_summary, :current_temperature, :city_state, :country, :time, :today_forecast, :tonight_forecast, :feels_like, :humidity, :visibility, :uv_index, :eight_hour_temperatures
 
   # :high_temperature, :low_temperature, :date
 
