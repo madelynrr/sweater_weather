e9800d756f17d52f94669aec42466c31338c338c
Madelyn Romero <m.romero013@gmail.com>
2020-03-02 11:50:56 -0700

    Create future forecast method

--- app/services/dark_sky_service.rb
+++ app/services/dark_sky_service.rb
@@ -1,12 +1,19 @@
 class DarkSkyService
 
-  def initialize(lat, long)
+  def initialize(lat, long, arrival_time = nil)
     @lat = lat
     @long = long
+    @arrival_time = arrival_time
   end
 
   def get_forecast
     Faraday.get("https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/#{@lat},#{@long}") do |req|
+      req.params['exclude'] = 'minutely'
+    end
+  end
+
+  def get_future_forecast
+    Faraday.get("https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/#{@lat},#{@long},#{@arrival_time}") do |req|
       req.params['exclude'] = 'minutely'
     end
   end
