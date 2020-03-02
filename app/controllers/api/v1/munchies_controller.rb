f1d7e62d1d30f0b483b064cfcc908c08f96103b2
Madelyn Romero <m.romero013@gmail.com>
2020-03-02 09:47:20 -0700

    Create Faraday call to google directions

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,6 +1,16 @@
 class Api::V1::MunchiesController < ApplicationController
 
   def index
+    start_point = params[:start]
+    end_point = params[:end]
+
+    duration_response = Faraday.get('https://maps.googleapis.com/maps/api/directions/json') do |req|
+      req.params['key'] = ENV['GOOGLE_DIRECTIONS_API_KEY']
+      req.params['origin'] = start_point
+      req.params['destination'] = end_point
+
+  end
+    require "pry"; binding.pry
   end
 
 end
