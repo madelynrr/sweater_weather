53f63db8d5129dd17f945ee9d74377173871d715
Madelyn Romero <m.romero013@gmail.com>
2020-03-01 23:39:50 -0700

    Comment out working code for refactor of weather response api call

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -3,25 +3,27 @@ class Api::V1::ForecastController < ApplicationController
   def index
     location = params[:location]
 
-    response = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |req|
-      req.params['key'] = ENV['GOOGLE_GEOCODING_API_KEY']
-      req.params['address'] = location
-    end
-
-    lat = JSON.parse(response.body)['results'].first['geometry']['location']['lat']
-    long = JSON.parse(response.body)['results'].first['geometry']['location']['lng']
-
-
-    weather_response = Faraday.get("https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/#{lat},#{long}") do |req|
-      req.params['exclude'] = 'minutely'
-    end
-
-    render json: weather_response.body
-
-
+    # response = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |req|
+    #   req.params['key'] = ENV['GOOGLE_GEOCODING_API_KEY']
+    #   req.params['address'] = location
+    # end
+    #
+    # lat = JSON.parse(response.body)['results'].first['geometry']['location']['lat']
+    # long = JSON.parse(response.body)['results'].first['geometry']['location']['lng']
+    #
+    #
+    # weather_response = Faraday.get("https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/#{lat},#{long}") do |req|
+    #   req.params['exclude'] = 'minutely'
+    # end
+    #
+    # render json: weather_response.body
 
 
 
+
+
+
+    render json: ForecastSerializer.new(ForecastFacade.new(location).forecast_results)
   end
 
 end
