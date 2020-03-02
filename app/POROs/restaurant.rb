6ce765e1a6990d615406bafce9643b8720b519f5
Madelyn Romero <m.romero013@gmail.com>
2020-03-02 12:11:12 -0700

    Refactor for restaurant name

--- app/POROs/restaurant.rb
+++ app/POROs/restaurant.rb
@@ -3,7 +3,8 @@ class Restaurant
   attr_reader :name, :address
 
   def initialize(attributes)
-    @name = ['name']
+    @name = attributes['name']
+    require "pry"; binding.pry
     @address = attributes['location']['display_address'].first + ' ' + attributes['location']['display_address'].second
 
   end
