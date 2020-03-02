cb592c8620e36b9dabcb76fc733979a59d292e54
Madelyn Romero <m.romero013@gmail.com>
2020-03-01 19:16:40 -0700

    Add lat and long to expects

--- spec/requests/api/v1/home_page_weather_spec.rb
+++ spec/requests/api/v1/home_page_weather_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Weather API" do
-  it "can retrieve lat and long with given city and state" do
-    get '/api/v1/forecast?location=denver,co'
+  it "can retrieve lat and long with given city and state", :vcr do
+    # VCR.use_cassette('denver_lat_long') do
 
-    expect(response).to be_successful
-    lat = JSON.parse(response.body)
+      get '/api/v1/forecast?location=denver,co'
 
-  end
+      expect(response).to be_successful
+      lat_and_long = JSON.parse(response.body)
+      expect(lat_and_long['lat']).to eq(39.7392358)
+      expect(lat_and_long['lng']).to eq(-104.990251)
+
+    end
 end
