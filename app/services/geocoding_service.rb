2541abf37c0066be4106a1e7061313043aa9c582
Madelyn Romero <m.romero013@gmail.com>
2020-03-02 01:21:16 -0700

    Create method for lat long call

--- app/services/geocoding_service.rb
+++ app/services/geocoding_service.rb
@@ -1,7 +1,13 @@
-# class GeocodingService
+class GeocodingService
 
-#
-#   # def location_lat
-#   #
-#   # end
-# end
+  def initialize(location)
+    @location = location
+  end
+
+  def get_lat_long
+    Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |req|
+      req.params['key'] = ENV['GOOGLE_GEOCODING_API_KEY']
+      req.params['address'] = @location
+    end
+  end
+end
