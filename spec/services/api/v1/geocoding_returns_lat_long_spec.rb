17a2f3b429b922db3ef9ddc46a0fbe7f495a2378
Madelyn Romero <m.romero013@gmail.com>
2020-03-02 08:17:28 -0700

    Create test for geocoding service to return lat, long, and country

--- spec/services/api/v1/geocoding_returns_lat_long_spec.rb
+++ spec/services/api/v1/geocoding_returns_lat_long_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Geocoding Service" do
-  it "returns lat by given city" do
+  it "returns lat by given city", :vcr do
     location = "denver,co"
-    service = GeocodingService.new(location)
-    lat = service.location_lat
+    lat_long_search = GeocodingService.new(location).get_lat_long
 
-    expect(lat).to eq(39.7392358)
+    parsed_response = JSON.parse(lat_long_search.body)
+
+    expect(parsed_response).to be_a Hash
+    expect(parsed_response['results']).to be_an Array
+    expect(parsed_response['results'].first.key?('geometry')).to be(true)
+    expect(parsed_response['results'].first['geometry']['location'].key?('lat')).to be(true)
+    expect(parsed_response['results'].first['geometry']['location'].key?('lng')).to be(true)
+    expect(parsed_response['results'].first['address_components'][3].key?('long_name')). to be(true)
   end
 end
