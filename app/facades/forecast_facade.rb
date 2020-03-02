3faf3f3d0b2fd20197b4801e2d1fad6e7a42d117
Madelyn Romero <m.romero013@gmail.com>
2020-03-02 01:52:45 -0700

    Delete hashed out code

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -13,40 +13,15 @@ class ForecastFacade
   end
 
   def daily_forecast
-
-    # response = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |req|
-    #   req.params['key'] = ENV['GOOGLE_GEOCODING_API_KEY']
-    #   req.params['address'] = location
-    # end
-    #
-    # lat = JSON.parse(response.body)['results'].first['geometry']['location']['lat']
-    # long = JSON.parse(response.body)['results'].first['geometry']['location']['lng']
-    # country = JSON.parse(response.body)['results'].first['address_components'][3]['long_name']
-
-
-    # weather_response = Faraday.get("https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/#{lat},#{long}") do |req|
-    #   req.params['exclude'] = 'minutely'
-    # end
-    #
-    # response_body = JSON.parse(weather_response.body)
-
-
-    daily_forecast = DailyForecast.new(forecast_information, location, country)
+    DailyForecast.new(forecast_information, location, country)
   end
 
   def forecast_information
-    # weather_response = Faraday.get("https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/#{lat},#{long}") do |req|
-    #   req.params['exclude'] = 'minutely'
-    # end
-
     JSON.parse(weather_response.body)
   end
 
   def weather_response
     DarkSkyService.new(lat, long).get_forecast
-    # weather_response = Faraday.get("https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/#{lat},#{long}") do |req|
-    #   req.params['exclude'] = 'minutely'
-    # end
   end
 
   def lat
@@ -63,10 +38,6 @@ class ForecastFacade
 
   def geocode_response
     GeocodingService.new(location).get_lat_long
-    # Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |req|
-    #   req.params['key'] = ENV['GOOGLE_GEOCODING_API_KEY']
-    #   req.params['address'] = location
-    # end
   end
 
 end
