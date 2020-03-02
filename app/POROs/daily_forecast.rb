3e0b22d375e5662f4c689d060f93b691f3462dda
Madelyn Romero <m.romero013@gmail.com>
2020-03-02 01:04:45 -0700

    Add attributes

--- app/POROs/daily_forecast.rb
+++ app/POROs/daily_forecast.rb
@@ -1,6 +1,7 @@
 class DailyForecast
 
-  attr_reader :id, :weather_icon, :current_summary, :current_temperature, :city_state, :country, :time
+  attr_reader :id, :weather_icon, :current_summary, :current_temperature, :city_state, :country, :time, :today_forecast, :tonight_forecast, :feels_like, :humidity, :visibility, :uv_index
+  # :high_temperature, :low_temperature, :date
 
   def initialize(attributes, location, country)
     @id = nil
@@ -12,10 +13,13 @@ class DailyForecast
     @city_state = location
     @country = country
     @time = attributes['currently']['time']
-
-
-
-
+    # @date = attributes
+    @today_forecast = attributes['daily']['summary']
+    @tonight_forecast = attributes['daily']['data'].first['summary']
+    @feels_like = attributes['currently']['apparentTemperature']
+    @humidity = attributes['currently']['humidity']
+    @visibility = attributes['currently']['visibility']
+    @uv_index = attributes['currently']['uvIndex']
   end
 
 end
