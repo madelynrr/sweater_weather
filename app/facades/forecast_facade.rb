3a6e7b726fa9c5cb6c6a6859521aa070af55a577
Madelyn Romero <m.romero013@gmail.com>
2020-03-01 23:30:01 -0700

    Move logic for both calls to forecast facade

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,6 +1,27 @@
 class ForecastFacade
 
+  # attr_reader :location
+
   def initialize(location)
+    @location = location
+  end
+
+  def forecast_results
+    response = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |req|
+      req.params['key'] = ENV['GOOGLE_GEOCODING_API_KEY']
+      req.params['address'] = location
+    end
+
+    lat = JSON.parse(response.body)['results'].first['geometry']['location']['lat']
+    long = JSON.parse(response.body)['results'].first['geometry']['location']['lng']
+
+
+    weather_response = Faraday.get("https://api.darksky.net/forecast/#{ENV['DARK_SKY_API_KEY']}/#{lat},#{long}") do |req|
+      req.params['exclude'] = 'minutely'
+    end
+
+    # render json: weather_response.body
+
   end
 
 end
