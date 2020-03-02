70edb7babb24a8daf01d6d0c077a529346dae0dc
Madelyn Romero <m.romero013@gmail.com>
2020-03-02 07:43:41 -0700

    Add high and low temperatures

--- app/POROs/daily_forecast.rb
+++ app/POROs/daily_forecast.rb
@@ -1,6 +1,6 @@
 class DailyForecast
 
-  attr_reader :id, :weather_icon, :current_summary, :current_temperature, :city_state, :country, :time, :today_forecast, :tonight_forecast, :feels_like, :humidity, :visibility, :uv_index, :eight_hour_temperatures, :five_day_forecast
+  attr_reader :id, :weather_icon, :current_summary, :current_temperature, :high_temperature, :low_temperature, :city_state, :country, :time, :today_forecast, :tonight_forecast, :feels_like, :humidity, :visibility, :uv_index, :eight_hour_temperatures, :five_day_forecast
   # :high_temperature, :low_temperature, :date
 
   def initialize(attributes, location, country)
@@ -9,8 +9,8 @@ class DailyForecast
     @weather_icon = attributes['currently']['icon']
     @current_summary = attributes['currently']['summary']
     @current_temperature = attributes['currently']['temperature']
-    # @high_temperature = attributes
-    # @low_temperature = attributes
+    @high_temperature = attributes['daily']['data'].first['temperatureHigh']
+    @low_temperature = attributes['daily']['data'].first['temperatureLow']
     @city_state = location
     @country = country
     @time = attributes['currently']['time']
