101ca63c351ee92a8a028de17af530d284e68308
Madelyn Romero <m.romero013@gmail.com>
2020-03-02 01:21:02 -0700

    Refactor for geocoding service

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -46,10 +46,11 @@ class ForecastFacade
   end
 
   def geocode_response
-    Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |req|
-      req.params['key'] = ENV['GOOGLE_GEOCODING_API_KEY']
-      req.params['address'] = location
-    end
+    GeocodingService.new(location).get_lat_long
+    # Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |req|
+    #   req.params['key'] = ENV['GOOGLE_GEOCODING_API_KEY']
+    #   req.params['address'] = location
+    # end
   end
 
 end
