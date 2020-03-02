2eb64e2b8e3a5b423effe8f1413fd03990cad4c5
Madelyn Romero <m.romero013@gmail.com>
2020-03-01 19:17:00 -0700

    Fix faraday call and pull out lat and long

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -3,13 +3,13 @@ class Api::V1::ForecastController < ApplicationController
   def index
     location = params[:location]
 
-    conn = Faraday.get(url: "https://maps.googleapis.com/maps/api/geocode/json") do |faraday|
-      faraday.params = {
-        key: ENV['GOOGLE_GEOCODING_API_KEY'],
-        address: location
-      }
+    response = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |req|
+      req.params['key'] = ENV['GOOGLE_GEOCODING_API_KEY']
+      req.params['address'] = location
     end
-require "pry"; binding.pry
+
+    lat_and_long = JSON.parse(response.body)['results'].first['geometry']['location']
+    render json: lat_and_long
   end
 
 end
