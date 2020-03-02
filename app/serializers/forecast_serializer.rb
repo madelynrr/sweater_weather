18f5c3454d9daf6a8c8d5783370984c1cbfd0965
Madelyn Romero <m.romero013@gmail.com>
2020-03-02 01:04:31 -0700

    Add attributes

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,6 +1,8 @@
 class ForecastSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :id, :weather_icon, :current_summary, :current_temperature, :city_state, :country, :time
+  attributes :id, :weather_icon, :current_summary, :current_temperature, :city_state, :country, :time, :today_forecast, :tonight_forecast, :feels_like, :humidity, :visibility, :uv_index
+
+  # :high_temperature, :low_temperature, :date
 
 end
