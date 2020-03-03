d93d11ebefed7ce43cdebb792f59694ddab11d03
Madelyn Romero <m.romero013@gmail.com>
2020-03-03 11:29:46 -0700

    Add hourly forecast to attributes

--- app/serializers/daily_forecast_serializer.rb
+++ app/serializers/daily_forecast_serializer.rb
@@ -1,7 +1,7 @@
 class DailyForecastSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :id, :current_forecast
+  attributes :id, :current_forecast, :hourly_forecast
   # :weather_icon, :current_summary, :current_temperature, :high_temperature, :low_temperature, :city_state, :country, :time, :today_forecast, :tonight_forecast, :feels_like, :humidity, :visibility, :uv_index, :eight_hour_temperatures, :five_day_forecast
 
   # :high_temperature, :low_temperature, :date
