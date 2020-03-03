ad1b2396bd20794e5962726c7334bb237d3b4bb9
Madelyn Romero <m.romero013@gmail.com>
2020-03-03 16:14:58 -0700

    Set up unsplash API call

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,15 +1,12 @@
 class Api::V1::BackgroundsController < ApplicationController
 
   def index
-    Faraday.get('https://api.unsplash.com/search/photos') do |req|
-      req.headers['Authorization'] = "Client-ID #{ENV['UNSPLASH_API_KEY']}"
-      # req.headers['Accept-Version'] = 'v1'
+    response = Faraday.get("https://api.unsplash.com/search/photos") do |req|
+      req.params['client_id'] = "#{ENV['UNSPLASH_API_KEY']}"
       req.params['query'] = 'denver'
     end
-
 require "pry"; binding.pry
     x = JSON.parse(response.body)
-
 
 
 
