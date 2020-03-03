45cc52cac1967b84df52ffd8f1e2d91db428651f
Madelyn Romero <m.romero013@gmail.com>
2020-03-03 14:20:33 -0700

    Create five day forecast method

--- app/POROs/daily_forecast.rb
+++ app/POROs/daily_forecast.rb
@@ -1,11 +1,21 @@
 class DailyForecast
 
   def initialize(attributes)
-    @daily_forecast = daily_forecast
+    @five_days = five_days(attributes)
   end
 
-  def five_days
-
+  def five_days(attributes)
+    acc = []
+    attributes['daily']['data'].each_with_index do |data, index|
+      hash = Hash.new
+      hash["date_#{index}"] = data["time"]
+      hash["icon_#{index}"] = data["icon"]
+      hash["summary_#{index}"] = data["summary"]
+      hash["precipitation_percentage_#{index}"] = data["precipProbability"]
+      hash["high_temperature_#{index}"] = data["temperatureHigh"]
+      hash["low_temperature_#{index}"] = data["temperatureLow"]
+      acc << hash
+    end
+    acc[0..4]
   end
-
 end
