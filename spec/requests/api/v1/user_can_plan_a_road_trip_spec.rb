f7cfe2c99bc554f4074fe8d91cbca2bb99d8ab68
Madelyn Romero <m.romero013@gmail.com>
2020-03-04 03:55:13 -0700

    Add expectations for response body

--- spec/requests/api/v1/user_can_plan_a_road_trip_spec.rb
+++ spec/requests/api/v1/user_can_plan_a_road_trip_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Weather API" do
   it "can return travel time and weather forecast for a road trip given an api key", :vcr do
-    WebMock.allow_net_connect!
-    VCR.eject_cassette
-    VCR.turn_off!
+    # WebMock.allow_net_connect!
+    # VCR.eject_cassette
+    # VCR.turn_off!
 
     user = User.create(email: "whatever@example.com",
                 "password": "password",
@@ -24,13 +24,13 @@ RSpec.describe "Weather API" do
     expect(response).to be_successful
 
     expect(parsed_response['data'].key?('type')).to be(true)
-    expect(parsed_response['data'].key?('type')).to eq("road_trip")
-    expect(parsed_response['data']['attributes'].key?('id')).to be(true)
-    expect(parsed_response['data']['attributes'].key?('origin')).to be(true)
-    expect(parsed_response['data']['attributes'].key?('destination')).to be(true)
-    expect(parsed_response['data']['attributes'].key?('travel_time')).to be(true)
-    expect(parsed_response['data']['attributes'].key?('travel_time')).to be(true)
-    expect(parsed_response['data']['attributes'].key?('icon')).to be(true)
-    expect(parsed_response['data']['attributes'].key?('temperature')).to be(true)
+    expect(parsed_response['data']['type']).to eq("road_trip")
+    expect(parsed_response['data']['attributes'].key?('road_trip')).to be(true)
+    expect(parsed_response['data']['attributes'].key?('travel_forecast')).to be(true)
+    expect(parsed_response['data']['attributes']['road_trip'].key?('origin')).to be(true)
+    expect(parsed_response['data']['attributes']['road_trip'].key?('destination')).to be(true)
+    expect(parsed_response['data']['attributes']['road_trip'].key?('travel_time_text')).to be(true)
+    expect(parsed_response['data']['attributes']['travel_forecast'].key?('summary')).to be(true)
+    expect(parsed_response['data']['attributes']['travel_forecast'].key?('temperature')).to be(true)
   end
 end
