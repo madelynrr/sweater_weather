73cd445ebdf5f4a527cea90331e5fe0bf037bd83
Madelyn Romero <m.romero013@gmail.com>
2020-03-03 17:56:39 -0700

    Refactor logic out to services and facades

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,15 +1,9 @@
 class Api::V1::BackgroundsController < ApplicationController
 
   def index
-    response = Faraday.get("https://api.unsplash.com/search/photos") do |req|
-      req.params['client_id'] = "#{ENV['UNSPLASH_API_KEY']}"
-      req.params['query'] = params['location'].split(',').first
-    end
+    location = params[:location].split(',').first
 
-    # background = BackgroundFacade.new()
-    #
-    # render json: BackgroundSerializer.new(background)
-
-    render json: JSON.parse(response.body)['results'].first['urls']['raw']
+    background = BackgroundFacade.new(location)
+    render json: BackgroundSerializer.new(background)
   end
 end
