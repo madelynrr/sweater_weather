e52a254eb747a5e747e18e9df81b26f97a485379
Madelyn Romero <m.romero013@gmail.com>
2020-03-03 17:40:40 -0700

    Use params for location

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -3,8 +3,12 @@ class Api::V1::BackgroundsController < ApplicationController
   def index
     response = Faraday.get("https://api.unsplash.com/search/photos") do |req|
       req.params['client_id'] = "#{ENV['UNSPLASH_API_KEY']}"
-      req.params['query'] = 'denver'
+      req.params['query'] = params['location'].split(',').first
     end
+
+    # background = BackgroundFacade.new()
+    #
+    # render json: BackgroundSerializer.new(background)
 
     render json: JSON.parse(response.body)['results'].first['urls']['raw']
   end
