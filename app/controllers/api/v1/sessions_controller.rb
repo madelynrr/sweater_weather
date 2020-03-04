45f89c29aba8b1227fd261a932904c94a32b4390
Madelyn Romero <m.romero013@gmail.com>
2020-03-03 20:57:45 -0700

    Create session controller and initial create action

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: UserSerializer.new(user)
+    end
+    # response.status = 
+  end
+
+end
