121009a9c7f9088ee804eab4521554b17a9811ef
Madelyn Romero <m.romero013@gmail.com>
2020-03-03 19:26:27 -0700

    Change test expectations for parsed response format

--- spec/requests/api/v1/creates_user_in_database_spec.rb
+++ spec/requests/api/v1/creates_user_in_database_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Weather API creates a user" do
-  it "creates a user and stores it in the database and create a unique API key" do
+  it "creates a user and stores it in the database and returns a unique API key" do
     user_params = {
                     "email": "whatever@example.com",
                     "password": "password",
@@ -10,9 +10,13 @@ RSpec.describe "Weather API creates a user" do
     post '/api/v1/users', params: user_params
 
     user = User.last
-
     expect(response).to be_successful
     expect(user.email).to eq(user_params[:email])
     expect(user.api_key?).to be(true)
+
+    parsed_response = JSON.parse(response.body)
+    expect(parsed_response['data']['attributes'].key?('api_key')).to be(true)
   end
+
+
 end
