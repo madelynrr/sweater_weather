567764633ad1d0be8c128bbef4a09521c73138bf
Madelyn Romero <m.romero013@gmail.com>
2020-03-04 02:02:41 -0700

    Add initial Faraday call to google maps api

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::RoadTripController < ApplicationController
 
   def create
-    require "pry"; binding.pry
+    origin = params[:origin]
+    destination = params[:destination]
+
+    duration_response = Faraday.get('https://maps.googleapis.com/maps/api/directions/json') do |req|
+      req.params['key'] = ENV['GOOGLE_DIRECTIONS_API_KEY']
+      req.params['origin'] = origin
+      req.params['destination'] = destination
+  end
+
+
 
   end
 
