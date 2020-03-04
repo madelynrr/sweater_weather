43f3f845d9773511d93116d6fd07d016b33486d8
Madelyn Romero <m.romero013@gmail.com>
2020-03-03 19:11:25 -0700

    Change test to create user and generate api key

--- spec/requests/api/v1/creates_user_in_database_spec.rb
+++ spec/requests/api/v1/creates_user_in_database_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Weather API creates a user" do
-  it "creates a user and stores it in the database and returns a unique API key" do
+  it "creates a user and stores it in the database and create a unique API key" do
     user_params = {
                     "email": "whatever@example.com",
                     "password": "password",
@@ -11,10 +11,8 @@ RSpec.describe "Weather API creates a user" do
 
     user = User.last
 
+    expect(response).to be_successful
     expect(user.email).to eq(user_params[:email])
     expect(user.api_key?).to be(true)
-
-    expect(response).to be_successful
-    expect(response.body.key?('api_key')).to be(true)
   end
 end
