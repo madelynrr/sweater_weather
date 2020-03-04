a8f588d285698e09af9b209c508dc4ce733c5d54
Madelyn Romero <m.romero013@gmail.com>
2020-03-03 20:14:13 -0700

    Add logic to return errors if user not saved

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-    if user = User.create!(user_params)
+    user = User.create(user_params)
+    if user.save
       response.status = 201
-      render json: UserSerializer.new(user)
+      render json: UserSerializer.new(User.last)
+    else
+      response.status = 400
+      # errors = user.errors.full_messages.to_sentence
+      render json: ErrorSerializer.new(user)
     end
   end
 
