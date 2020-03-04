6b9c2b1cd5c29902f10ad5a2578975eb50aa4276
Madelyn Romero <m.romero013@gmail.com>
2020-03-03 17:56:03 -0700

    Fix test expectations

--- spec/requests/api/v1/returns_url_for_home_page_based_on_city_spec.rb
+++ spec/requests/api/v1/returns_url_for_home_page_based_on_city_spec.rb
@@ -4,8 +4,9 @@ RSpec.describe "Weather API Homepage" do
   it "returns url by give city and state", :vcr do
     get '/api/v1/backgrounds?location=denver,co'
 
+    parsed_response = JSON.parse(response.body)
+
     expect(response).to be_successful
-
-
+    expect(parsed_response['data']['attributes'].key?('image_url')).to eq(true)
   end
 end
