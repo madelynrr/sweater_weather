8d5daf85a269d3202e6676c50ef39b56bd0cd843
Madelyn Romero <m.romero013@gmail.com>
2020-03-04 01:58:51 -0700

    Add expectations and params to test for creating a road trip

--- spec/requests/api/v1/user_can_plan_a_road_trip_spec.rb
+++ spec/requests/api/v1/user_can_plan_a_road_trip_spec.rb
@@ -2,8 +2,25 @@ require 'rails_helper'
 
 RSpec.describe "Weather API" do
   it "can return travel time and weather forecast for a road trip" do
-    post "/api/v1/road_trip"
+    params = {
+              "origin": "Denver,CO",
+              "destination": "Pueblo,CO",
+              "api_key": "jgn983hy48thw9begh98h4539h4"
+              }
+    post "/api/v1/road_trip", params: params
+
+    parsed_response = JSON.parse(response.body)
+
     expect(response).to be_successful
 
+    expect(parsed_response['data'].key?('type')).to be(true)
+    expect(parsed_response['data'].key?('type')).to eq("road_trip")
+    expect(parsed_response['data']['attributes'].key?('id')).to be(true)
+    expect(parsed_response['data']['attributes'].key?('origin')).to be(true)
+    expect(parsed_response['data']['attributes'].key?('destination')).to be(true)
+    expect(parsed_response['data']['attributes'].key?('travel_time')).to be(true)
+    expect(parsed_response['data']['attributes'].key?('travel_time')).to be(true)
+    expect(parsed_response['data']['attributes'].key?('icon')).to be(true)
+    expect(parsed_response['data']['attributes'].key?('temperature')).to be(true)
   end
 end
